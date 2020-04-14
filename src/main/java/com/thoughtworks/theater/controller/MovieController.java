62123c2a4b86de83d7739c4605d2b2f4eb3855d6
UI-Mario <JZ.Livioo@gmail.com>
2020-04-14 12:07:25 +0800

    [LJZ]: add func getByTag

--- src/main/java/com/thoughtworks/theater/controller/MovieController.java
+++ src/main/java/com/thoughtworks/theater/controller/MovieController.java
@@ -10,53 +10,58 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MovieController {
 
-  private final MovieService movieService;
+    private final MovieService movieService;
 
-  public MovieController(MovieService movieService) {
-    this.movieService = movieService;
-  }
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
 
-  @GetMapping("/movie/in_theaters")
-  public Iterable<Movie> getMoviesInTheaters() {
-    // TODO: implement this method
-    return null;
-  }
+    @GetMapping("/movie/in_theaters")
+    public Iterable<Movie> getMoviesInTheaters() {
+        // TODO: implement this method
+        return null;
+    }
 
-  @GetMapping("/movie/new_movies")
-  public Iterable<Movie> getNewMovies() {
-    // TODO: implement this method
-    return null;
-  }
+    @GetMapping("/movie/new_movies")
+    public Iterable<Movie> getNewMovies() {
+        // TODO: implement this method
+        return null;
+    }
 
-  @GetMapping("/movie/top250")
-  public Iterable<Movie> getTop250Movies() {
-    // TODO: implement this method
-    return null;
-  }
+    @GetMapping("/movie/top250")
+    public Iterable<Movie> getTop250Movies() {
+        // TODO: implement this method
+        return null;
+    }
 
-  @GetMapping("/movie/top250?start={start}&count={count}")
-  public Iterable<Movie> getTop250Movies(@PathVariable int start, @PathVariable int count) {
-    // TODO implement this method
-    return null;
-  }
+    @GetMapping("/movie/top250?start={start}&count={count}")
+    public Iterable<Movie> getTop250Movies(@PathVariable int start, @PathVariable int count) {
+        // TODO implement this method
+        return null;
+    }
 
-  @GetMapping("/movie/search?keyword={keyword}")
-  public Iterable<Movie> searchMovies(@PathVariable String keyword) {
-    // TODO: implement this method
-    // 这里我没考虑清楚诶。。如果用户输入多个关键字用空格隔开，这里该怎么写
-    return null;
-  }
+    @GetMapping("/movie/search?keyword={keyword}")
+    public Iterable<Movie> searchMovies(@PathVariable String keyword) {
+        // TODO: implement this method
+        // 这里我没考虑清楚诶。。如果用户输入多个关键字用空格隔开，这里该怎么写
+        return null;
+    }
 
-  @GetMapping("/movie/similar_movies?movieId={movieId}")
-  public Iterable<Movie> getSimilarMovies(@PathVariable String movieId) {
-    // TODO: implement this method
-    return null;
-  }
+    @GetMapping("/movie/similar_movies?movieId={movieId}")
+    public Iterable<Movie> getSimilarMovies(@PathVariable String movieId) {
+        // TODO: implement this method
+        return null;
+    }
 
-  @GetMapping("/movie/detail/{movieId}")
-  public Movie getStudent(@PathVariable String movieId) {
-    // TODO: implement this method
-    return null;
-  }
+    @GetMapping("/movie/detail/{movieId}")
+    public Movie getMovieDetail(@PathVariable String movieId) {
+        // TODO: implement this method
+        return null;
+    }
+
+    @GetMapping("/movie/tags={tag}")
+    public Movie gitMovieByTag(@PathVariable String tag) {
+        return null;
+    }
 
 }
